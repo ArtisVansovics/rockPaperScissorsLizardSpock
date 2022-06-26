b5b1f624cc198991f7012f222517f7c9eb3500c1
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-26 19:38:33 +0300

    Created PrimaryButton

---
+++ src/components/PrimaryButton/PrimaryButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="button" :disabled="isDisabled" @click="clickHandler">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PrimaryButton',
+  props: {
+    isDisabled: Boolean,
+  },
+  emits: ['on-click'],
+  methods: {
+    clickHandler() {
+      this.$emit('on-click');
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./PrimaryButton.scss" />
