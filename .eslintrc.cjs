1c57b2de6d57864dc0df01534f08affa26d89444
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-26 13:55:07 +0300

    Modified .eslintrc.cjs to avoid Eslint and Prettier conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,11 @@ module.exports = {
   root: true,
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
   ],
   env: {
     "vue/setup-compiler-macros": true,
