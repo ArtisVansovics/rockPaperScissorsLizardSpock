0afd8a5e5919cc00504bc911262509bc4005aa87
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-26 20:47:41 +0300

    Created ButtonImages array containing names and urls of every image

---
+++ src/data/buttonImages.ts
@@ -0,0 +1,31 @@
+import type ButtonImage from '@/models/ButtonImageModel';
+
+const buttonImages: ButtonImage[] = [
+  {
+    id: 1,
+    imgUrl: 'src/assets/images/rock.png',
+    imgName: 'Rock',
+  },
+  {
+    id: 2,
+    imgUrl: 'src/assets/images/paper.png',
+    imgName: 'Paper',
+  },
+  {
+    id: 3,
+    imgUrl: 'src/assets/images/scissors.png',
+    imgName: 'Scissors',
+  },
+  {
+    id: 4,
+    imgUrl: 'src/assets/images/lizard.png',
+    imgName: 'Lizard',
+  },
+  {
+    id: 5,
+    imgUrl: 'src/assets/images/spock.png',
+    imgName: 'Spock',
+  },
+];
+
+export default buttonImages;
