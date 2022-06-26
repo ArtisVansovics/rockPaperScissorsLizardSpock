bc824341cffa651aa601519b6b903ecf32952e76
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-26 20:49:18 +0300

    Created RoundButton to display available player options as clickable images

---
+++ src/components/RoundButton/RoundButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button" :disabled="isDisabled" @click="clickHandler">
+    <img class="img" :src="imgUrl" :alt="imgName" />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RoundButton',
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    imgUrl: {
+      type: String,
+      default: '',
+    },
+    imgName: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: ['on-click'],
+  methods: {
+    clickHandler() {
+      this.$emit('on-click');
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./RoundButton.scss" />
